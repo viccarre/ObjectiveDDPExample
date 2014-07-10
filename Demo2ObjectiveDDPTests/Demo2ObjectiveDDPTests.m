f8208ccb8fd6269862b6da782cbbda9a5e5f9fc2
Victor Carreño <viccarre@Victors-MacBook-Pro.local>
2014-07-10 09:43:30 -0500

    Initial Commit

---
+++ Demo2ObjectiveDDPTests/Demo2ObjectiveDDPTests.m
@@ -0,0 +1,34 @@
+//
+//  Demo2ObjectiveDDPTests.m
+//  Demo2ObjectiveDDPTests
+//
+//  Created by Victor Carreño on 7/10/14.
+//  Copyright (c) 2014 Victor Carreño. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Demo2ObjectiveDDPTests : XCTestCase
+
+@end
+
+@implementation Demo2ObjectiveDDPTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
